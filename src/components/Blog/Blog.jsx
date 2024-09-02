23c668ce67bbf116b1bbb91f632ca030c6f70a41
FarHan949 <uddinfarhan949@gmail.com>
2024-09-02 12:05:52 +0600

    added showing props loop throuw in map

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,33 @@
 import PropTypes from 'prop-types'
 
 const Blog = ({blog}) => {
+
+    const {cover, title, author, author_img, reading_time, posted_date, hashtags} = blog
     return (
         <div>
+            <img src={cover} alt={`cover picture for the title ${title}`} />
             
+            <div className='flex justify-between'>
+                <div className='flex gap-3'>
+                        <img className='w-14 rounded-3xl' src={author_img} alt="" /> 
+                        <div className=''>
+                            <h3 className='text-2xl'>{author}</h3>
+                            <p>{posted_date}</p>
+                        </div>
+                </div>
+                <div>
+                    <div>
+                          <span>{reading_time} min read</span>
+                    </div>
+                </div>
+            </div>
+
+            <h1 className='text-3xl'>{title}</h1>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { data } from 'autoprefixer';
 import React, { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
 
@@ -14,6 +15,10 @@ useEffect(()=>{
     return (
         <div className='md:w-2/3'>
             <div className="text-4xl">Blogs</div>
+            {
+                blogs.map(blog => <Blog key={blog.id}
+                blog={blog}></Blog>)
+            }
         </div>
     );
 };
